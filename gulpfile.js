3322fc84732a6d123189f833acb2ac46c579af42
John Rizzo <johnrizzo1@gmail.com>
2016-01-04 00:08:24 -0500

    done with logging users in

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp');
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['build'], function () {
-});
+gulp.task('default', ['build'], function () {});
 
 gulp.task('build', function () {
   return gulp.src('static/javascripts/**/*.js')
---
+++ static/javascripts/authentication/authentication.module.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jrizzo on 1/3/16.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.authentication', [
+      'thinkster.authentication.controllers',
+      'thinkster.authentication.services'
+    ]);
+
+  angular
+    .module('thinkster.authentication.controllers', []);
+
+  angular
+    .module('thinkster.authentication.services', ['ngCookies']);
+})();
